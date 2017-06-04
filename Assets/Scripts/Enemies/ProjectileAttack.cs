2883d9e41b2e7ea9e3ec891bb88c1f688b755b16
Kamila Wysocka <kam.wysocka@gmail.com>
2017-06-04 19:56:12 +0200

    placeholder projectile attack, example enemies prefabs ready

---
+++ Assets/Scripts/Enemies/ProjectileAttack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileAttack : BaseAttackBehaviour {
+	Coroutine fightCoroutine;
+
+	void Start () {
+		myself.onStateChange.AddListener (HandleStateChange);
+	}
+
+	void HandleStateChange() {
+		if (myself.state == Enemy.State.Fight) {
+			fightCoroutine = StartCoroutine (Fight());
+		} else if (fightCoroutine != null) {
+			StopCoroutine (fightCoroutine);
+		}
+	}
+
+	IEnumerator Fight() {
+		while (true) {
+			myself.player.GetDamage (GetRandomAttackPower ());
+			yield return new WaitForSeconds (attacksTimeOffset);
+		}
+	}
+
+	float GetRandomAttackPower() {
+		return Random.Range(minAttackPower, maxAttackPower);
+	}
+}
