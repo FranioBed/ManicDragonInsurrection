661e6b9f1ef092c624e8d731f5efeb974d8e37a5
merk <m.marekf1@gmail.com>
2017-06-12 14:19:40 +0200

    Spawned random item from chest.

--- Assets/Scripts/ObjectsOnLevel/LootChest.cs
+++ Assets/Scripts/ObjectsOnLevel/LootChest.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class LootChest : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GameObject instance = Instantiate(Resources.Load("Prefabs/Items/UsableItemPrefab")) as GameObject;
+            instance.transform.position = gameObject.transform.position;
+            Destroy(gameObject);
+        }
+            
+    }
 }
